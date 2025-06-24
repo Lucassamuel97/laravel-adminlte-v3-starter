3204400c78054eb0d8501a69cfa5b7a47da29142
Lucassamuel97 <lukassamuka88@gmail.com>
2025-06-24 19:28:42 -0300

    :sparkles: feat: Atualiza a página inicial para exibir contagem de produtos e usuários cadastrados

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +25,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        // Contar produtos e usuários
+        $totalProducts = Product::count();
+        $totalUsers = User::count();
+
+        // Passar os dados para a view
+        return view('home', compact('totalProducts', 'totalUsers'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,16 +2,25 @@
 
 {{-- Customize layout sections --}}
 
-@section('subtitle', 'Welcome')
+@section('subtitle', 'Dashboard')
+
+{{-- Set the title and subtitle for the content header --}}
 @section('content_header_title', 'Home')
-@section('content_header_subtitle', 'Welcome')
+@section('content_header_subtitle', 'Dashboard')
 
 {{-- Content body: main page content --}}
 
 @section('content_body')
-    <p>Welcome to this beautiful admin panel.</p>
-
-   
+    <div class="row">
+        {{-- Total de Produtos --}}
+        <div class="col-lg-3 col-6">
+            <x-adminlte-info-box title="Produtos Cadastrados" text="{{ $totalProducts }}" icon="fas fa-box" theme="primary" url="{{ route('products.index') }}" url-text="Ver Produtos"/>
+        </div>
+        {{-- Total de Usuários --}}
+        <div class="col-lg-3 col-6">
+            <x-adminlte-info-box title="Usuários Cadastrados" text="{{ $totalUsers }}" icon="fas fa-users" theme="info" url="{{ route('users.index') }}" url-text="Gerenciar Usuários" />
+        </div>
+    </div>
 @stop
 
 {{-- Push extra CSS --}}
@@ -23,6 +32,6 @@
 
 {{-- Push extra scripts --}}
 
-@push('js')
+<!-- @push('js')
     <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
-@endpush
+@endpush -->
